64c03d1625076a67e1649e3f35d277ecfc957649
sinhake2203 <rakeshmmmec@gmail.com>
2019-12-11 00:50:23 +0530

    commit2

--- CodingPractices-Java/src/com/java/coding/threading/futuretask/prec1/FutureTaskTestClient.java
+++ CodingPractices-Java/src/com/java/coding/threading/futuretask/prec1/FutureTaskTestClient.java
@@ -3,7 +3,6 @@ package com.java.coding.threading.futuretask.prec1;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
 /*
