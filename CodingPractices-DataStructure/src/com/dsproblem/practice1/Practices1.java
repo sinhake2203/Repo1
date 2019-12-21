80a32f3093a7986fd4ad9d890ee1cd1ee4123f5d
sinhake2203 <rakeshmmmec@gmail.com>
2019-12-21 21:17:25 +0530

    remove sonar issue

--- CodingPractices-DataStructure/src/com/dsproblem/practice1/Practices1.java
+++ CodingPractices-DataStructure/src/com/dsproblem/practice1/Practices1.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 public class Practices1 {
 	
-	private final static Logger LOGGER = Logger.getLogger(Practices1.class.getName());
+	private  static final Logger LOGGER = Logger.getLogger(Practices1.class.getName());
 
 	public static void main(String[] args) {
 		
